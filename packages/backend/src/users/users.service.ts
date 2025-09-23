a6d07632d5f165b9f221681973b0488e940ffdfa
TomMo <morettithomas54@gmail.com>
2025-09-24 01:42:13 +0200

    create user service functions

--- packages/backend/src/users/users.service.ts
+++ packages/backend/src/users/users.service.ts
@@ -1,6 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './user.schema';
 
 @Injectable()
 export class UsersService {
+  constructor(@InjectModel(User.name) private readonly userModel: Model<User>) {}
 
+  async createUser(email: string, password: string, lastname: string, firstname: string, type: string): Promise<User> {
+    const newUser = new this.userModel({ email, password, lastname, firstname, type });
+    return newUser.save();
+  }
+
+  async findUserById(id: string): Promise<User | null> {
+    return this.userModel.findById(id).exec();
+  }
+
+  async updateUser(id: string, updateData: Partial<User>): Promise<User | null> {
+    return this.userModel.findByIdAndUpdate(id, updateData, { new: true }).exec();
+  }
+
+  async deleteUser(id: string): Promise<User | null> {
+    return this.userModel.findByIdAndDelete(id).exec();
+  }
 }
