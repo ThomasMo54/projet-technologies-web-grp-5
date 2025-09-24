4192533c5f29859647a54a5c2e9f6d902e001b0f
TomMo <morettithomas54@gmail.com>
2025-09-24 13:13:30 +0200

    create auth module, service and routes

--- packages/backend/src/users/users.controller.ts
+++ packages/backend/src/users/users.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common'
 import { UsersService } from './users.service';
 import { User } from './user.schema';
 import { CreateUserDto } from "./dto/create-user.dto";
-import {UpdateUserDto} from "./dto/update-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
 
 @Controller('users')
 export class UsersController {
