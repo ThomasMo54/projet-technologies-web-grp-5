d6a4394c44502565bdf06bb3084522a00983d8df
TomMo <morettithomas54@gmail.com>
2025-09-24 12:11:14 +0200

    add data validation to user routes

--- packages/backend/src/users/users.controller.ts
+++ packages/backend/src/users/users.controller.ts
@@ -1,24 +1,26 @@
 import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './user.schema';
+import { CreateUserDto } from "./dto/create-user.dto";
+import {UpdateUserDto} from "./dto/update-user.dto";
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  async create(@Body() body: { email: string; password: string; lastname: string; firstname: string; type: string }) {
-    return this.usersService.createUser(body.email, body.password, body.lastname, body.firstname, body.type);
+  async create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto.email, createUserDto.password, createUserDto.lastname, createUserDto.firstname, createUserDto.type);
   }
 
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<User | null> {
-    return this.usersService.findUserById(id);
+    return this.usersService.findUserByIdWithoutPassword(id);
   }
 
   @Put(':id')
-  async update(@Param('id') id: string, @Body() body: Partial<User>): Promise<User | null> {
-    return this.usersService.updateUser(id, body);
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto): Promise<User | null> {
+    return this.usersService.updateUser(id, updateUserDto);
   }
 
   @Delete(':id')
--- packages/backend/src/users/users.service.ts
+++ packages/backend/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
@@ -9,20 +9,32 @@ export class UsersService {
   constructor(@InjectModel(User.name) private readonly userModel: Model<User>) {}
 
   async createUser(email: string, password: string, lastname: string, firstname: string, type: string): Promise<User> {
+    const existingUser = await this.findUserByEmail(email);
+    if (existingUser) {
+      throw new ConflictException('User with this email already exists');
+    }
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new this.userModel({ email, password: hashedPassword, lastname, firstname, type });
     return newUser.save();
   }
 
   async findUserById(id: string): Promise<User | null> {
-    return this.userModel.findById(id).exec();
+    return this.userModel.findOne({ uuid: id }).exec();
+  }
+
+  async findUserByIdWithoutPassword(id: string): Promise<User | null> {
+    return this.userModel.findOne({ uuid: id }).select('-password').exec();
+  }
+
+  async findUserByEmail(email: string): Promise<User | null> {
+    return this.userModel.findOne({ email }).exec();
   }
 
   async updateUser(id: string, updateData: Partial<User>): Promise<User | null> {
-    return this.userModel.findByIdAndUpdate(id, updateData, { new: true }).exec();
+    return this.userModel.findOneAndUpdate({ uuid: id }, updateData, { new: true }).select('-password').exec();
   }
 
   async deleteUser(id: string): Promise<User | null> {
-    return this.userModel.findByIdAndDelete(id).exec();
+    return this.userModel.findOneAndDelete({ uuid: id }).select('-password').exec();
   }
 }
